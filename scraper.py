8733bad61e1cb76bf3d2f878a5cc7b025db9d70b
Joe Cipolla <jlcipolla@gmail.com>
2019-12-08 08:19:18 -0600

    realtime

--- scraper.py
+++ scraper.py
@@ -5,18 +5,18 @@ import shutil
 root = '/Users/joecipolla/Dropbox/Reference/Project_Seldon/Data/NYISO/'
 data_type = 'realtime'  # 'damlbmp'
 
-# download archived zip files
-y = 2000
-while y < 2020:
-    m = 1
-    while m < 13:
-        tag = str(y) + str(m).zfill(2) + '01' + data_type + '_zone_csv.zip'
-        url = 'http://mis.nyiso.com/public/csv/' + data_type + '/' + tag
-        myfile = requests.get(url)
-        open(root + data_type + '/' + tag, 'wb').write(myfile.content)
-        print(tag)
-        m += 1
-    y += 1
+# # download archived zip files
+# y = 2000
+# while y < 2020:
+#     m = 1
+#     while m < 13:
+#         tag = str(y) + str(m).zfill(2) + '01' + data_type + '_zone_csv.zip'
+#         url = 'http://mis.nyiso.com/public/csv/' + data_type + '/' + tag
+#         myfile = requests.get(url)
+#         open(root + data_type + '/' + tag, 'wb').write(myfile.content)
+#         print(tag)
+#         m += 1
+#     y += 1
 
 
 # manually unzip each file
